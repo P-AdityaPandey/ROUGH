8bc81823327eea5cb49d211987ed246fedc880e9
P-AdityaPandey98 <adityapandeymfsd@gmail.com>
2026-03-29 20:50:13 +0530

    tested

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   < Counter/>
+    </>
+  )
+}
+
+export default App;
---
+++ counter-app/src/counter/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './Counter.css'
+
+function Counter() {
+    var count = 
+
+
+  return (
+   <div id='counter'>
+    <h2>COUNTER</h2>
+    <p>The Count is {state}</p>
+    <div>
+        <button>INCREASE</button>
+        <button>DECRESE</button>
+        <button>RESET</button>
+    </div>
+
+   </div>
+  )
+}
+
+export default Counter;
